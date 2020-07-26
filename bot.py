470918ddc3d7ce949e31169524789771959a1366
Deathlike-rest <alex1588025@gmail.com>
2020-07-26 23:35:52 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,4 +20,18 @@ bot = commands.Bot(command_prefix='!')
 async def test(ctx, arg): #создаем асинхронную фунцию бота
     await ctx.send(arg) #отправляем обратно аргумент
 
-bot.run(TOKEN)w
+bot.run(TOKEN)
+
+import discord
+from discord.ext import commands
+
+TOKEN = 'NzM3MDE2MTQ5NTU2MDY4NDAy.Xx3Nxw.7ufaMQaIDNbSl8EMRb5u6PFTKQw'
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(pass_context=True) #разрешаем передавать агрументы
+async def test(ctx, arg): #создаем асинхронную фунцию бота
+    await ctx.send(arg) #отправляем обратно аргумент
+
+bot.run(TOKEN)
+
+
